de0c94f4c96297fff0d941586cc15b96f5836689
Julia Nye <jujaritchie@gmail.com>
2024-02-27 12:04:59 -0700

    Added some images and routing for nav bar links.

--- routes/index.js
+++ routes/index.js
@@ -7,4 +7,19 @@ router.get('/', (req, res) => {
     res.sendFile(filePath);
 });
 
+router.get('/stories', (req, res) => {
+    const filePath = path.resolve(__dirname, "../public/stories.html")
+    res.sendFile(filePath);
+});
+
+router.get('/contact', (req, res) => {
+    const filePath = path.resolve(__dirname, "../public/contact.html")
+    res.sendFile(filePath);
+});
+
+router.get('/script', (req, res) => {
+    const filePath = path.resolve(__dirname, "../public/script.html")
+    res.sendFile(filePath);
+});
+
 module.exports = router;
